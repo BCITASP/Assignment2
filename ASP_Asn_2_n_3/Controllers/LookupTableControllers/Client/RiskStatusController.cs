962e091f1277232f977aad63abe458697618fbac
TheRocktopus <matthewdbanman@gmail.com>
2015-03-02 21:42:58 -0800

    Added controllers for lookup tables and new bootstrap theme. Links to lookup table data added to dropdown nav menu

---
+++ ASP_Asn_2_n_3/Controllers/LookupTableControllers/Client/RiskStatusController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP_Asn_2_n_3.Models;
+
+namespace ASP_Asn_2_n_3.Controllers.LookupTableControllers.Client
+{
+    public class RiskStatusController : Controller
+    {
+        private GoodSamaritanContext db = new GoodSamaritanContext();
+
+        // GET: RiskStatus
+        public ActionResult Index()
+        {
+            return View(db.RiskStatus.ToList());
+        }
+
+        // GET: RiskStatus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RiskStatus riskStatus = db.RiskStatus.Find(id);
+            if (riskStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(riskStatus);
+        }
+
+        // GET: RiskStatus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RiskStatus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RiskStatusId,Status")] RiskStatus riskStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RiskStatus.Add(riskStatus);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(riskStatus);
+        }
+
+        // GET: RiskStatus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RiskStatus riskStatus = db.RiskStatus.Find(id);
+            if (riskStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(riskStatus);
+        }
+
+        // POST: RiskStatus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RiskStatusId,Status")] RiskStatus riskStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(riskStatus).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(riskStatus);
+        }
+
+        // GET: RiskStatus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RiskStatus riskStatus = db.RiskStatus.Find(id);
+            if (riskStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(riskStatus);
+        }
+
+        // POST: RiskStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RiskStatus riskStatus = db.RiskStatus.Find(id);
+            db.RiskStatus.Remove(riskStatus);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASP_Asn_2_n_3/Controllers/LookupTableControllers/Client/StatusOfFilesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP_Asn_2_n_3.Models;
+
+namespace ASP_Asn_2_n_3.Controllers.LookupTableControllers.Client
+{
+    public class StatusOfFilesController : Controller
+    {
+        private GoodSamaritanContext db = new GoodSamaritanContext();
+
+        // GET: StatusOfFiles
+        public ActionResult Index()
+        {
+            return View(db.StatusOfFiles.ToList());
+        }
+
+        // GET: StatusOfFiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StatusOfFile statusOfFile = db.StatusOfFiles.Find(id);
+            if (statusOfFile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(statusOfFile);
+        }
+
+        // GET: StatusOfFiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StatusOfFiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StatusOfFileId,Status")] StatusOfFile statusOfFile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StatusOfFiles.Add(statusOfFile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(statusOfFile);
+        }
+
+        // GET: StatusOfFiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StatusOfFile statusOfFile = db.StatusOfFiles.Find(id);
+            if (statusOfFile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(statusOfFile);
+        }
+
+        // POST: StatusOfFiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StatusOfFileId,Status")] StatusOfFile statusOfFile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(statusOfFile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(statusOfFile);
+        }
+
+        // GET: StatusOfFiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StatusOfFile statusOfFile = db.StatusOfFiles.Find(id);
+            if (statusOfFile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(statusOfFile);
+        }
+
+        // POST: StatusOfFiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StatusOfFile statusOfFile = db.StatusOfFiles.Find(id);
+            db.StatusOfFiles.Remove(statusOfFile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
