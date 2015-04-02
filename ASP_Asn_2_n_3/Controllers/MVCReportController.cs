53701bff4b3ccaadd4f5bf62f25d57d8deacdd59
alan-cy-lai <alan@ergostratus.com>
2015-04-02 16:26:32 -0700

    MVC Report Month and Year selection complete

---
+++ ASP_Asn_2_n_3/Controllers/MVCReportController.cs
@@ -0,0 +1,36 @@
+﻿using ASP_Asn_2_n_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Asn_2_n_3.Controllers
+{
+    public class MVCReportController : Controller
+    {
+        // GET: MVCReport
+        public ActionResult Index()
+        {
+            GoodSamaritanContext ctx = new GoodSamaritanContext();
+
+            IEnumerable<SelectListItem> siYears = (from c in ctx.FiscalYears
+                                                   select new { c.FiscalYearId, c.Years }).AsEnumerable()
+                                                   .Select(m => new SelectListItem()
+                                                   {
+                                                       Value = m.FiscalYearId.ToString(),
+                                                       Text = m.Years
+                                                   });
+            ViewBag.yearsSelect = new SelectList(siYears, "Value", "Text");
+            return View();
+        }
+
+        // POST: MVCReport/DisplayReport
+        [HttpPost]
+        public ActionResult Report()
+        {
+
+            return View();
+        }
+    }
+}
