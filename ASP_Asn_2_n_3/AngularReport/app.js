c0f87584a022ded3841f902a35c12d1b5c8f8ff3
alan-cy-lai <alan@ergostratus.com>
2015-04-01 23:03:43 -0700

    Month and Year working, Report design completed TODO: Write query and hook it all up

--- ASP_Asn_2_n_3/AngularReport/app.js
+++ ASP_Asn_2_n_3/AngularReport/app.js
@@ -7,6 +7,7 @@
         var port = "57290";
         var baseURL = "http://" + host + ":" + port + "/";
         var loginURL = baseURL + "Token";
+        var apiURL = baseURL + "api/Report/";
 
         $scope.performLogin = function () {
             var data = "&grant_type=password&" + "username=" + $scope.username + "&password=" + $scope.password;
@@ -20,17 +21,52 @@
                 .success(function (response) {
                     $scope.report = response;
                     $scope.showlogin = false;
-                    $scope.showreport = true;
+                    $scope.showMonthYear = true;
                 })
                 .error(function (response) {
                     $scope.loginerror = response.error_description;
                 });
+        }
+
+        var getMonthYear = function () {            
+            $http.get(apiURL + "getyear")
+            .success(function (response) {
+                var json = JSON.stringify(response);
+                var parser = JSON.parse(json);
+                $scope.yearData = response;
+            });
+        }
+
+        $scope.loadReport = function () {
+            var month = $scope.selectedMonth;
+            var yearID = $scope.selectedYear;
+            if (typeof month === "undefined" || typeof yearID === "undefined") { return; }
+            if (month.length >= 0 && yearID.length >= 0) {
+                retrieveReport(month, yearID);
+            }
+            $scope.month = $("#selectMonth>option:selected").html();
+            $scope.year = $("#selectYear>option:selected").html();
 
 
+            var m_names = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September",
+                "October", "November", "December");
+            var d = new Date();
+            $scope.currentdate = m_names[d.getMonth()] + " " + d.getDate() + "/" + d.getFullYear();
+        }
+
+        var retrieveReport = function (monthNum, yearID) {
+            $http.get(apiURL + "getReport?monthnum=" + monthNum + "&yearid=" + yearID)
+                .success(function (response) {
+
+                });
         }
 
         $scope.showlogin = true;
+        getMonthYear();
+        $scope.showMonthYear = true;
+        $scope.showreport = true;
     };
+
 
     app.controller("ReportController", ["$scope", "$http", ReportController]);
 }());
--- ASP_Asn_2_n_3/Controllers/ReportController.cs
+++ ASP_Asn_2_n_3/Controllers/ReportController.cs
@@ -4,16 +4,40 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ASP_Asn_2_n_3.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
 
 namespace ASP_Asn_2_n_3.Controllers
 {
     public class ReportController : ApiController
     {
-        // GET api/report
-        [Authorize]
-        public IEnumerable<string> Get()
+        public class YearObj
         {
-            return new string[] { "value1", "value2" };
+            public int id;
+            public string year;
+        }
+
+
+        // GET api/report/getyear
+        public IEnumerable<YearObj> GetYear()
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            GoodSamaritanContext ctx = new GoodSamaritanContext();
+            List<YearObj> yearList = new List<YearObj>();
+            var qry = (from c in ctx.FiscalYears
+                      select new { c.FiscalYearId, c.Years });
+            foreach (var item in qry)
+            {
+                yearList.Add(new YearObj { id = item.FiscalYearId, year = item.Years });
+            }
+            return yearList;
+        }
+
+        [HttpGet]
+        public string getReport(int monthnum, int yearid)
+        {
+            return monthnum + " : " + yearid;
         }
     }
 }
